f94b3339a5770ee26997d57d406c96931e7a3556
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-30 07:54:21 -1000

    Update Find-missing.js

--- Find-missing.js
+++ Find-missing.js
@@ -24,3 +24,5 @@ let findMissing = function (list) {
   let step = (list[list.length - 1] - list[0]) / (list.length);
   return list.filter(function(val, index) { return val !== (list[0] + index * step); })[0] - step;
 }
+
+// or
